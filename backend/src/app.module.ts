4172fc642c3a154a3b1fb79fa43e2dd47a82778f
ChoeDaeBeom <chleoqja1760@gmail.com>
2022-01-17 22:28:16 +0900

    nest serve static vue

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static'; // New
+import { join } from 'path'; // New
 
+const root = join(__dirname, '..', '../frontend/dist');
+console.log(root);
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: root,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
