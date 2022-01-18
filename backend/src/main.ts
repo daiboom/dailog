96ab7359eef0347e054eb7d71c2c4ae57b425cf2
Daiboom <chleoqja0125@nate.com>
2022-01-18 16:20:48 +0900

    remove semi

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
 
-  await app.listen(3000);
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
